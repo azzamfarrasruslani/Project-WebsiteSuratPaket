7fe73de1684a62fdf26c3796cc47e9aa61c83d9b
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2024-07-16 14:20:05 +0700

    Perbaikan pada side navbar untuk admin dan user

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -24,8 +24,6 @@ class Controller
     }
 
 
-
-
     public function view($view, $data = [])
     {
         extract($data);
--- app/Views/dashboard/layout/header.php
+++ app/Views/dashboard/layout/header.php
@@ -157,14 +157,16 @@ $isActive = $data['isActive'];
         </a>
     </div>
 
-    <hr class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
+    <hr
+        class="h-px mt-0 bg-transparent bg-gradient-to-r from-transparent via-black/40 to-transparent dark:bg-gradient-to-r dark:from-transparent dark:via-white dark:to-transparent" />
 
     <div class="items-center block w-auto max-h-screen overflow-auto grow basis-full">
         <ul class="flex flex-col pl-0 mb-0">
             <li class="mt-0.5 w-full">
                 <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors dark:text-white dark:opacity-80 <?= $isActive('dashboard/dashboard') ?>"
                     href="<?= BASE_URL; ?>dashboard/dashboard">
-                    <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                    <div
+                        class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                         <i class="relative top-0 text-sm leading-normal text-blue-500 ni ni-tv-2"></i>
                     </div>
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Dashboard</span>
@@ -178,23 +180,26 @@ $isActive = $data['isActive'];
             <li class="mt-0.5 w-full">
                 <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap  px-4  transition-colors dark:text-white dark:opacity-80 <?= $isActive('SerahTerima/dataBarang') ?>"
                     href="<?= BASE_URL; ?>SerahTerima/dataBarang">
-                    <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                    <div
+                        class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                         <i class="fa-solid fa-table" style="color: #fb6340;"></i>
                     </div>
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Data Paket</span>
                 </a>
             </li>
-
-            <li class="mt-0.5 w-full">
-                <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors dark:text-white dark:opacity-80 <?= $isActive('User/dataUser') ?>"
-                    href="<?= BASE_URL; ?>User/dataUser">
-                    <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                        <i class="fa-solid fa-users-rectangle" style="color: #63E6BE;"></i>
-                    </div>
-                    <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Data User</span>
-                </a>
-            </li>
-
+            <!-- Tampiilkan jika role admin -->
+            <?php if ($_SESSION['user_role'] !== 'user'): ?>
+                <li class="mt-0.5 w-full">
+                    <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors dark:text-white dark:opacity-80 <?= $isActive('User/dataUser') ?>"
+                        href="<?= BASE_URL; ?>User/dataUser">
+                        <div
+                            class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="fa-solid fa-users-rectangle" style="color: #63E6BE;"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Data User</span>
+                    </a>
+                </li>
+            <?php endif; ?>
             <li class="w-full mt-4">
                 <h6 class="pl-6 ml-2 text-xs font-bold leading-tight uppercase dark:text-white opacity-60">Akun</h6>
             </li>
@@ -202,7 +207,8 @@ $isActive = $data['isActive'];
             <li class="mt-0.5 w-full">
                 <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors dark:text-white dark:opacity-80 <?= $isActive('dashboard/profile') ?>"
                     href="<?= BASE_URL; ?>User/profile">
-                    <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                    <div
+                        class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                         <i class="relative top-0 text-sm leading-normal text-slate-700 ni ni-single-02"></i>
                     </div>
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Profile</span>
@@ -212,7 +218,8 @@ $isActive = $data['isActive'];
                 <a class="py-2.7 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap px-4 transition-colors dark:text-white dark:opacity-80 <?= $isActive('/auth/logout') ?>"
                     href="<?= BASE_URL; ?>/auth/logout" onclick="return confirm('Apakah anda yakin ingin keluar?')">
 
-                    <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                    <div
+                        class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                         <i class="fa-solid fa-right-from-bracket" style="color: #f55540;"></i>
                     </div>
                     <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Sign Out</span>
@@ -221,9 +228,3 @@ $isActive = $data['isActive'];
         </ul>
     </div>
 </aside>
-
-
-
-
-
-
