c4a83ce5eacd553c2007e6cdf292484f61ce86c5
azzamfarrasruslani <109759017+azzamfarrasruslani@users.noreply.github.com>
2024-07-18 19:29:25 +0700

    Menambahkan foto pada tabel user

--- app/Views/dashboard/dataUser.php
+++ app/Views/dashboard/dataUser.php
@@ -31,7 +31,18 @@
               <tbody>
                 <?php foreach ($security as $index => $item): ?>
                   <tr>
-                    <td class="text-sm"><?= $item['id_security'] ?></td>
+                    <td class="text-sm whitespace-nowrap">
+                      <div class="flex px-2 py-1">
+                        <div>
+                          <img src="../assets/img/team-2.jpg"
+                            class="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl"
+                            alt="user1" />
+                        </div>
+                        <div class="flex flex-col justify-center">
+                          <?= $item['id_security'] ?>
+                        </div>
+                      </div>
+                    </td>
 
                     <td class="capitalize text-sm"><?= $item['nama_security'] ?></td>
 
